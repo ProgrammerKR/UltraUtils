25c3d25266fab9ccb6dd6f6d127571c3441eb1e7
Prog. Kanishk Raj <programmerkr.123@gmail.com>
2025-03-25 09:53:41 +0530

    Create __init__.py

---
+++ UltraUtils/__init__.py
@@ -0,0 +1,10 @@
+from .strings import *
+from .files import *
+from .mathutils import *
+from .datetimeutils import *
+from .listutils import *
+from .dictutils import *
+from .randomutils import *
+from .systemutils import *
+
+__version__ = "1.0.0"
